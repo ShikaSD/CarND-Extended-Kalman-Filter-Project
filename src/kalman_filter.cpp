d857fd8bee85c57f4402b3ed3ab98121be2b267a
ashikov <andrei.shikov@zalando.fi>
2017-07-01 02:33:10 +0300

    Implement filter

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -1,4 +1,7 @@
 #include "kalman_filter.h"
+#include "tools.h"
+#include <math.h>
+#include <iostream>
 
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
@@ -9,31 +12,38 @@ KalmanFilter::~KalmanFilter() {}
 
 void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
                         MatrixXd &H_in, MatrixXd &R_in, MatrixXd &Q_in) {
-  x_ = x_in;
-  P_ = P_in;
-  F_ = F_in;
-  H_ = H_in;
-  R_ = R_in;
-  Q_ = Q_in;
+    x_ = x_in;
+    P_ = P_in;
+    F_ = F_in;
+    H_ = H_in;
+    R_ = R_in;
+    Q_ = Q_in;
 }
 
 void KalmanFilter::Predict() {
-  /**
-  TODO:
-    * predict the state
-  */
+    x_ = F_ * x_;
+    P_ = F_ * P_ * F_.transpose() + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
-  /**
-  TODO:
-    * update the state by using Kalman Filter equations
-  */
+    VectorXd y = z - H_ * x_;
+    UpdateInternal(y);
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
-  /**
-  TODO:
-    * update the state by using Extended Kalman Filter equations
-  */
+    H_ = Tools::CalculateJacobian(x_);
+    VectorXd y = z - Tools::FromCartesian(x_);
+    y[1] = atan2(sin(y[1]), cos(y[1]));
+    UpdateInternal(y);
+}
+
+void KalmanFilter::UpdateInternal(const VectorXd &y) {
+    MatrixXd Ht = H_.transpose();
+    MatrixXd S = H_ * P_ * Ht + R_;
+    MatrixXd Si = S.inverse();
+    MatrixXd K =  P_ * Ht * Si;
+    MatrixXd I = MatrixXd::Identity(x_.size(), x_.size());
+
+    x_ = x_ + (K * y);
+    P_ = (I - K * H_) * P_;
 }
--- src/tools.cpp
+++ src/tools.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include "tools.h"
 
+#define eps 1e-4
+
 using Eigen::VectorXd;
 using Eigen::MatrixXd;
 using std::vector;
@@ -11,15 +13,60 @@ Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
-  /**
-  TODO:
-    * Calculate the RMSE here.
-  */
+    VectorXd rmse(4);
+    rmse << 0,0,0,0;
+
+    if (estimations.size() != ground_truth.size()) {
+        return rmse;
+    }
+
+    //accumulate squared residuals
+    for(unsigned int i=0; i < estimations.size(); ++i){
+        VectorXd residual = estimations[i] - ground_truth[i];
+
+        //coefficient-wise multiplication
+        residual = residual.array()*residual.array();
+        rmse += residual;
+    }
+
+    //calculate the mean
+    rmse /= estimations.size();
+    //calculate the squared root
+    rmse = rmse.array().sqrt();
+
+    return rmse;
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
-  /**
-  TODO:
-    * Calculate a Jacobian here.
-  */
+    MatrixXd Hj(3,4);
+
+    double px = x_state(0);
+    double py = x_state(1);
+    double vx = x_state(2);
+    double vy = x_state(3);
+
+    double ps = sqrt(px * px + py * py);
+    double p2 = px * px + py * py;
+    double p3 = ps * ps * ps;
+
+    if (ps < eps) {
+        return Hj;
+    }
+    //compute the Jacobian matrix
+    Hj << px / ps, py / ps, 0, 0,
+            -py / (p2), px / (p2), 0, 0,
+            (py * (vx * py - vy * px)) / p3, (px * (vy * px - vx * py)) / p3, px / ps, py / ps;
+
+    return Hj;
+}
+
+VectorXd Tools::FromCartesian(const VectorXd &x) {
+    cout <<"X: " <<x <<endl;
+
+    VectorXd z = VectorXd(3);
+    double rho = sqrt(x[0] * x[0] + x[1] * x[1]);
+    z << rho, atan2(x[1], x[0]), (x[0] * x[2] + x[1] * x[3]) / rho;
+    cout <<"Z: " <<z <<endl;
+
+    return z;
 }
